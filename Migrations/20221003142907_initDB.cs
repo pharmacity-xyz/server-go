b23ae9cd13c8b3d699426457faf1850de85ee94d
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-04 10:06:37 +0700

    Update

---
+++ Migrations/20221003142907_initDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pharmacity_v1.Migrations
+{
+    public partial class initDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
