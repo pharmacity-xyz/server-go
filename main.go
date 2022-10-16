09745dfe22f648d2ebdb25ed0c6930de6740ce28
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-16 23:06:29 +0700

    Update json

--- main.go
+++ main.go
@@ -38,4 +38,30 @@ func main() {
 	}
 
 	log.Println("unmarchalled: %v", unmarshalled)
+
+	// write json from a struct
+	var mySlice []Person
+
+	var m1 Person
+	m1.FirstName = "Wally"
+	m1.LastName = "West"
+	m1.HairColor = "red"
+	m1.HasDog = false
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+	m2.FirstName = "Diana"
+	m2.LastName = "Prince"
+	m2.HairColor = "black"
+	m2.HasDog = false
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "	")
+	if err != nil {
+		log.Println("error marchalling", err)
+	}
+
+	log.Println(string(newJson))
 }
