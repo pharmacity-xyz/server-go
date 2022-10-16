b6b70a456de715f542b5d60762e98c859e83ed23
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-16 20:09:37 +0700

    For loop

--- main.go
+++ main.go
@@ -1,47 +1,9 @@
 package main
 
-import (
-	"log"
-)
-
-type User struct {
-	FirstName string
-	LastName  string
-}
-
-type myStruct struct {
-	FirstName string
-}
-
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
-}
+import "log"
 
 func main() {
-	myVar := "hello"
-
-	switch myVar {
-	case "cat":
-		log.Println("cat")
-
-	case "dog":
-		log.Println("dog")
-
-	case "fish":
-		log.Println("fish")
-
-	default:
-		log.Println("does not match")
+	for i := 0; i <= 10; i++ {
+		log.Println(i)
 	}
-}
-
-func saySomething(s string) (string, string) {
-	log.Println("s from the saySomething func is", s)
-	return s, "World"
-}
-
-func changeUsingPointer(s *string) {
-	log.Println("s is set to", s)
-	newValue := "Red"
-	*s = newValue
 }
