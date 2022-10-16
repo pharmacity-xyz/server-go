2d751a3b6f8b292a255626b367f0b0cb202bba1d
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-16 17:20:37 +0700

    Update

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ var s = "seven"
 
 func main() {
 	var s2 = "six"
+	s := "eight"
 
 	log.Println("s is ", s)
 	log.Println("s2 is ", s2)
+
+	saySomething(s)
 }
 
 func saySomething(s string) (string, string) {
+	log.Println("s from the saySomething func is", s)
 	return s, "World"
 }
 
