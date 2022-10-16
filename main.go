2593886373a003e858a4e4ef6e8e6d9e7d41c64d
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-16 20:31:32 +0700

    interface

--- main.go
+++ main.go
@@ -1,22 +1,40 @@
 package main
 
-import "log"
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
 
 func main() {
-	type User struct {
-		FirstName string
-		LastName  string
-		Email     string
-		Age       uint
+	dog := Dog{
+		Name:  "Samson",
+		Breed: "German Shephered",
 	}
 
-	var users []User
-	users = append(users, User{"John", "Smith", "john@gmail.com", 14})
-	users = append(users, User{"John", "Smith", "john@gmail.com", 14})
-	users = append(users, User{"John", "Smith", "john@gmail.com", 14})
-	users = append(users, User{"John", "Smith", "john@gmail.com", 14})
+	PrintInfo(dog)
+}
 
-	for _, l := range users {
-		log.Println(l.FirstName)
-	}
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+func (d Dog) Says() string {
+	return "Woof"
+}
+
+func (d Dog) NumberOfLegs() int {
+	return 4
 }
