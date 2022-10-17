48c30e993ed81a2aaaec201bb5170cdff10e1c2c
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-17 11:31:29 +0700

    Update

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 )
 
-var portNumber = ":8080"
+const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	_, _ = fmt.Fprintf(w, "This is the home page")
 }
 
 // About is the home page handler
@@ -29,5 +29,7 @@ func addValues(x, y int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
 }
