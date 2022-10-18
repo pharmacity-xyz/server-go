23bc7375c2ff187c24d01e6dd90291adff67dcd1
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-18 17:00:41 +0700

    Update rpc

--- main.go
+++ main.go
@@ -2,52 +2,34 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
-	"os"
-	"strconv"
+	"net/rpc"
 	"time"
-
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 )
 
-type city struct {
-	Name string
-	Area uint64
-}
+type Args struct{}
 
-func filterContentType(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Currently in the check content type middleware")
-		if r.Header.Get("Content-type") != "application/json" {
-			w.WriteHeader(http.StatusUnsupportedMediaType)
-			w.Write([]byte("415 - Unsupported Media Type. Please send JSON"))
-			return
-		}
-		handler.ServeHTTP(w, r)
+type TimeServer int64
 
-	})
-}
-
-func setServerTimeCookie(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-		cookie := http.Cookie{Name: "Servier-Time(UTC)", Value: strconv.FormatInt(time.Now().Unix(), 10)}
-
-		http.SetCookie(w, &cookie)
-		log.Println("Currently in the set server time middleware")
-	})
-}
-
-func mainLogic(w http.ResponseWriter, r *http.Request) {
-	log.Println("Processing request!")
-	w.Write([]byte("OK"))
-	log.Println("Finished processing request")
+func (t *TimeServer) GiveServerTime(args *Args, reply *int64) error {
+	*reply = time.Now().Unix()
+	return nil
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", mainLogic)
-	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
-	http.ListenAndServe(":8000", loggedRouter)
+	timeServer := new(TimeServer)
+	err := rpc.Register(timeServer)
+	if err != nil {
+		log.Fatal("error")
+	}
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	errorS := http.Serve(l, nil)
+	if errorS != nil {
+		log.Fatal("error")
+	}
 }
