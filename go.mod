cc084b962c201ac925e8da391c804d048c02facc
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-18 09:15:48 +0700

    Update main function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pharmacity-xyz/server
 
 go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"math/rand"
+	"log"
 	"net/http"
+	"os/exec"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func main() {
-	newMux := http.NewServeMux()
-	newMux.HandleFunc("/randomFloat", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, rand.Float64())
-	})
-	newMux.HandleFunc("/randomInt", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, rand.Intn(100))
-	})
+func getCommandOutput(command string, arguments ...string) string {
+	cmd := exec.Command(command, arguments...)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(fmt.Sprint(err) + ": " + stderr.String())
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(fmt.Sprint(err) + ": " + stderr.String())
+	}
+	return out.String()
 
-	http.ListenAndServe(":8000", newMux)
+}
+
+func goVersion(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Fprintf(w, getCommandOutput("/usr/bin/go", "version"))
+}
+
+func getFileContent(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Fprintf(w, getCommandOutput("/bin/cat", params.ByName("name")))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/api/v1/go-version", goVersion)
+	router.GET("/api/v1/show-file/:name", getFileContent)
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
