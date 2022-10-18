868c172018923d34ec8eebe4add3412d57fe8e19
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-18 08:45:10 +0700

    Update main function

--- main.go
+++ main.go
@@ -1,41 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"html"
+	"io"
+	"log"
 	"net/http"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/pharmacity-xyz/server/romanNumerals"
 )
 
+func MyServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		urlPathElements := strings.Split(r.URL.Path, "")
+	http.HandleFunc("/hello", MyServer)
 
-		if urlPathElements[1] == "roman_number" {
-			number, _ := strconv.Atoi(strings.TrimSpace(urlPathElements[2]))
-			if number == 0 || number > 10 {
-				w.WriteHeader(http.StatusNotFound)
-				w.Write([]byte("404 - Not Found"))
-
-			} else {
-				fmt.Fprintf(w, "%q", html.EscapeString(romanNumerals.Numerals[number]))
-			}
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("400 - Bad request"))
-		}
-	})
-
-	s := &http.Server{
-		Addr:           ":8000",
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	s.ListenAndServe()
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
