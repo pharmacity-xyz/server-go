201749466fced5d131ed51d0c1b5514e4b7198b6
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-18 16:47:39 +0700

    Update middleware

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/pharmacity-xyz/server
 go 1.19
 
 require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
-func middleware(handler http.Handler) http.Handler {
+type city struct {
+	Name string
+	Area uint64
+}
+
+func filterContentType(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Executing middleware before request phase!")
+		log.Println("Currently in the check content type middleware")
+		if r.Header.Get("Content-type") != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Write([]byte("415 - Unsupported Media Type. Please send JSON"))
+			return
+		}
 		handler.ServeHTTP(w, r)
-		fmt.Println("Executing middleware after response phase!")
+
+	})
+}
+
+func setServerTimeCookie(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+		cookie := http.Cookie{Name: "Servier-Time(UTC)", Value: strconv.FormatInt(time.Now().Unix(), 10)}
+
+		http.SetCookie(w, &cookie)
+		log.Println("Currently in the set server time middleware")
 	})
 }
 
 func mainLogic(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Executing mainHandler...")
-	w.Write([]byte("Ok"))
+	log.Println("Processing request!")
+	w.Write([]byte("OK"))
+	log.Println("Finished processing request")
 }
 
 func main() {
-	mainLogicHandler := http.HandlerFunc(mainLogic)
-	http.Handle("/", middleware(mainLogicHandler))
-	http.ListenAndServe(":8000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", mainLogic)
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	http.ListenAndServe(":8000", loggedRouter)
 }
