ee68ca3988237a91f0aa226931ca1037681e2896
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-26 23:14:36 +0700

    Create user table

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Libraries
 ### Routing
 - [chi](https://github.com/go-chi/chi)
+
+### Migratioin
+- [goose](https://github.com/pressly/goose)
+
+### Developer tool
+- [modd](https://github.com/cortesi/modd)
+- [adminer](https://github.com/vrana/adminer)
---
+++ migrations/20221126221630_users.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users(
+    user_id uuid PRIMARY KEY,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    city TEXT NOT NULL,
+    country TEXT NOT NULL,
+    company_name TEXT NOT NULL,
+    role TEXT NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+-- +goose StatementEnd
